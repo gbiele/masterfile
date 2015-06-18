dfd2299db19f0e614fa91147fed65186b7e8358b
gbiele <guidopb@neuro-cognition.org>
2015-06-18 16:42:48 +0200

    add script that loads data with ADHD scale from Q6

---
+++ scripts4tests/ADHD_SCALE_Q6.R
@@ -0,0 +1,9 @@
+get_ADHD_SCALE_Q6 = function(){
+  dt = data.table(read_sav("savs/Atferd.sav"))
+  
+  old_names = names(dt)[grep("^AT3",names(dt))]
+  new_names = paste("ADHD_screening_scale.item",1:11,sep = "")
+  setnames(dt,old_names,new_names)
+  dt$ADHD_screening_scale.sumSCORE = make_sum_scores(dt[,new_names,with = F])
+  return(dt[,c("PREG_ID_299","BARN_NR",new_names,"ADHD_screening_scale.sumSCORE"),with = F])
+}
