6ac1850c5b63f67a634d24f3b23051382b663683
gbiele <guidopb@neuro-cognition.org>
2015-06-18 16:44:02 +0200

    directly merge loaded data, add some error corrections that independent of PRED_ID number, add some missing questionnaires

--- collect_data.R
+++ collect_data.R
@@ -17,17 +17,22 @@ rm(tmp,file.sources)
 # - Spin the pots
 # - grooved pegboard
 # - 
-dt = get_neuropsych()
+MASTER = get_neuropsych()
 
 #####################################################
 ################## PAPA interview ###################
 #####################################################
-PAPA = get_PAPA()
+MASTER = merge(MASTER,get_PAPA(),by = c("PREG_ID_299","BARN_NR"),all = T)
 
 #####################################################
 ################## Stanford Binet ###################
 #####################################################
-PAPA = get_StanfordBinet()
+MASTER = merge(MASTER,get_ADHD_SCALE_Q6(),by = c("PREG_ID_299","BARN_NR"),all = T)
+
+#####################################################
+# screeing scale from Q6, filled out in ADHD Study ##
+#####################################################
+MASTER = merge(MASTER,get_StanfordBinet(),by = c("PREG_ID_299","BARN_NR"),all = T)
 
 
 #####################################################
@@ -37,12 +42,12 @@ PAPA = get_StanfordBinet()
 pqa = data.table(read_sav("savs/SBF.sav"))
 pqb = data.table(read_sav("savs/ADHD13_SBF.sav"))
 
-cdi = get_cdi(pqa,pqb,"parent")
-sdq = get_sdq(pqa,pqb,"parent")
-brief = get_brief(pqa,pqb,"parent")
-cbq = get_cbq_eas(pqa,pqb) 
-child_diags = get_diagnoses(pqa,pqb)
-fam_health = get_family_illness(pqa,pqb)
+MASTER = merge(MASTER,get_cdi(pqa,pqb,"parent"),by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_sdq(pqa,pqb,"parent"),by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_brief(pqa,pqb,"parent"),by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_cbq_eas(pqa,pqb) ,by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_diagnoses(pqa,pqb),by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_family_illness(pqa,pqb),by = c("PREG_ID_299","BARN_NR"),all = T)
 
 rm(pqa,pqb)
 #####################################################
@@ -52,8 +57,38 @@ rm(pqa,pqb)
 kgqa = data.table(read_sav("savs/BHG.sav"))
 kgqb = data.table(read_sav("savs/ADHD6_BHG.sav"))
 
-cdi_kg = get_cdi_kg(kgqa,kgqb)
-sdq_kg = get_sdq(kgqa,kgqb,"teacher")
-brief_kg = get_brief(kgqa,kgqb,"teacher")
+MASTER = merge(MASTER,get_cdi_kg(kgqa,kgqb),by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_sdq(kgqa,kgqb,"teacher"),by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_brief(kgqa,kgqb,"teacher"),by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_Conners(kgqa,kgqb),by = c("PREG_ID_299","BARN_NR"),all = T)
+MASTER = merge(MASTER,get_Copland(kgqa,kgqb),by = c("PREG_ID_299","BARN_NR"),all = T)
 rm(kgqa,kgqb)
 # no cbq in kindergarden cbq = get_cbq_eas(pqa,pqb)
+
+
+
+################# corrections #######################
+# GENERELT: slette barn med PREG_ID_299 = 50163 fra alle tester, grunnet usikkerhet rundt barnets norskkunnskaper (dette er inkludert i alle endelige syntakser). 
+# Mor oversetter nær sagt alle testinstruksjoner til serbisk, vi har ikke kontroll på hva hun sier.
+is50163 = which(MASTER$Age_in_days == 1215 & MASTER$PAPA.ADHD.sum.SCORE == 24)
+MASTER = MASTER[-is50163,]
+
+# Vær obs på sakene 50163 og 87831 når disse syntaksene kjøres. Ingen av disse sakene skal ha valid ABIQ!!
+is87831 = which(MASTER$Age_in_days == 1294 & MASTER$PAPA.ADHD.sum.SCORE == 6 & MASTER$PAPA.BH.ODD.sum.SCORE == 8)
+
+MASTER[is87831, StBn.SCORE.ABIQ := NA]
+MASTER[is87831, StBn.SCORE.PercRank := NA]
+MASTER[is87831, StBn.SCORE.WMindex := NA]
+
+# Nina: slette BNT skåre til barnet der mor oversetter alle testinstruksjonene.
+MASTER[is87831,BNT.SCORE := NA]
+
+# bnt$BNT.SCORE = rowSums(bnt[,names(bnt)[grep("BN1_",names(bnt))],with = F] < 5)
+# bnt[PREG_ID_299 == 50163, BNT.SCORE := NA] 
+
+rm(is87831,is50163)
+
+
+save(MASTER,file = "masterfile.Rdata")
+MASTER_scores = MASTER[,grep("SCORE",names(MASTER)),with = F]
+save(MASTER_scores,file = "masterfile_scores.Rdata")
