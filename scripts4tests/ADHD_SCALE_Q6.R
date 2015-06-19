a5d22a72ca10fe16eae1d90691e6b1d4e4cfdb94
gbiele <guidopb@neuro-cognition.org>
2015-06-19 13:12:39 +0200

    make sure that ADHD sumscore has a minimum of 0

--- scripts4tests/ADHD_SCALE_Q6.R
+++ scripts4tests/ADHD_SCALE_Q6.R
@@ -5,5 +5,6 @@ get_ADHD_SCALE_Q6 = function(){
   new_names = paste("ADHD_screening_scale.item",1:11,sep = "")
   setnames(dt,old_names,new_names)
   dt$ADHD_screening_scale.sumSCORE = make_sum_scores(dt[,new_names,with = F])
+  dt[,ADHD_screening_scale.sumSCORE := ADHD_screening_scale.sumSCORE-min(ADHD_screening_scale.sumSCORE)]
   return(dt[,c("PREG_ID_299","BARN_NR",new_names,"ADHD_screening_scale.sumSCORE"),with = F])
 }
