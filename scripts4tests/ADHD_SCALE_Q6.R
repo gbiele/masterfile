be52960c9f3545f0c5ed49e61e33e9d1e0c1ce38
gbiele <guidopb@neuro-cognition.org>
2015-08-26 11:26:53 +0200

    adapt to new make_sum_score function

--- scripts4tests/ADHD_SCALE_Q6.R
+++ scripts4tests/ADHD_SCALE_Q6.R
@@ -2,9 +2,9 @@ get_ADHD_SCALE_Q6 = function(){
   dt = data.table(read_sav("savs/Atferd.sav"))
   
   old_names = names(dt)[grep("^AT3",names(dt))]
-  new_names = paste("ADHD_screening_scale.item",1:11,sep = "")
+  new_names = paste("ADHD.MOBAQ3.SCALE",1:11,sep = "")
   setnames(dt,old_names,new_names)
-  dt$ADHD_screening_scale.sumSCORE = make_sum_scores(dt[,new_names,with = F])
-  dt[,ADHD_screening_scale.sumSCORE := ADHD_screening_scale.sumSCORE-min(ADHD_screening_scale.sumSCORE)]
-  return(dt[,c("PREG_ID_299","BARN_NR",new_names,"ADHD_screening_scale.sumSCORE"),with = F])
+  dt = make_sum_scores(dt,new_names,"ADHD.MOBAQ3.SCALE.SS")
+  dt[,ADHD.MOBAQ3.SCALE.SS := ADHD.MOBAQ3.SCALE.SS-min(ADHD.MOBAQ3.SCALE.SS)]
+  return(dt[,c("PREG_ID_299","BARN_NR",new_names,"ADHD.MOBAQ3.SCALE.SS"),with = F])
 }
